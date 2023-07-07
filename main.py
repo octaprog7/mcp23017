d5d67dbae285a99dc058361d1512515b9492d6a7
octaprog7 <goctaprog@gmail.com>
2023-07-07 14:55:00 +0500

    ...

---
+++ main.py
@@ -0,0 +1,79 @@
+# micropython
+# mail: goctaprog@gmail.com
+# MIT license
+import time
+# Please read MCP23017 datasheet!
+from machine import I2C, Pin
+import mcp23017mod
+from sensor_pack.bus_service import I2cAdapter
+
+# Если в обработчике прерывания возникнет ошибка, MicroPython не сможет создать сообщение о ней!
+# Cоздаю специальный буфер для этой цели!
+# micropython.alloc_emergency_exception_buf(100)
+# interrupt counter, один байт
+# irq_cnt = bytearray(1)
+
+
+# def irq_handler(p: Pin):
+#    """Пожалуйста прочитай это: http://docs.micropython.org/en/latest/reference/isr_rules.html#isr-rules"""
+#    global irq_cnt
+#    irq_cnt[0] += 1
+
+
+if __name__ == '__main__':
+    # пожалуйста установите выводы scl и sda в конструкторе для вашей платы, иначе ничего не заработает!
+    # please set scl and sda pins for your board, otherwise nothing will work!
+    # https://docs.micropython.org/en/latest/library/machine.I2C.html#machine-i2c
+    # i2c = I2C(0, scl=Pin(13), sda=Pin(12), freq=400_000) # для примера
+    # bus =  I2C(scl=Pin(4), sda=Pin(5), freq=100000)   # на esp8266    !
+    # Внимание!!!
+    # Замените id=1 на id=0, если пользуетесь первым портом I2C !!!
+    # Warning!!!
+    # Replace id=1 with id=0 if you are using the first I2C port !!!
+    # i2c = I2C(0, scl=Pin(13), sda=Pin(12), freq=400_000) # для примера
+
+    # функции irq_handler будет передаваться управление, когда один из выводов P0..P7, настроенных, как ВХОД,
+    # изменит состояние! Вы можете подключить к выводу GPIO21 провод и подключать его к GND,
+    # чтобы прерывание возникло!
+    # Не забудьте подключить вывод INT PCF8574 к выводу GPIO21 платы Arduino Nano RP2040 Connect with RP2040 !!!
+
+    # i2c = I2C(id=1, scl=Pin(27), sda=Pin(26), freq=400_000)  # on Arduino Nano RP2040 Connect tested
+    i2c = I2C(id=1, scl=Pin(7), sda=Pin(6), freq=400_000)  # create I2C peripheral at frequency of 400kHz
+    adapter = I2cAdapter(i2c)
+
+
+if __name__ == '__main__':
+    expander = mcp23017mod.MCP23017(adapter)
+    # b = expander._read(0x10)
+    # print(b)
+    print(16 * "_")
+    ba, bb = expander._read_reg(0), expander._read_reg(1)
+    print(ba[0], bb[0])
+    v = expander._read_reg(0, 2)
+    print(f"v={v}, {v[0]}, {v[1]}")
+    # b = expander._read(0x10)
+    # print(b)
+    print(16 * "_")
+    # for i in range(0x10):
+    #    b = expander._read_reg(i)
+    #    print(f"{i}; {b}")
+    # expander.active_port = 0
+    # print(f"hex mode: {expander.hex_mode}")
+    # expander.io_dir = 0xFF	# 8 bit as input
+    # expander.pull_up = 0xFF		# connect 8 pull up resistors
+    # expander.input_polarity = 0xFF		# GPIO register bit reflects the same logic state of the input pin.
+    # print(f"iodir: 0x{expander.io_dir:X}")
+    # print(f"pull up: 0x{expander.pull_up:X}")
+    # print(f"active_port: {expander.active_port}")
+    # print(f"input polarity: 0x{expander.input_polarity:X}")
+    
+    for port in range(2):
+        expander.active_port = port
+        print(f"qqq active_port: {expander.active_port}")
+        expander.io_dir = 0xFF	# 8 bit as input
+        expander.pull_up = 0xFF		# connect 8 pull up resistors
+        expander.input_polarity = 0x00		# GPIO register bit reflects the same logic state of the input pin.
+    
+    while True:
+        time.sleep_ms(500)
+        print(f"{expander.gpio:X}")
---
+++ mcp23017mod.py
@@ -0,0 +1,300 @@
+# micropython
+# MIT license
+# Copyright (c) 2023 Roman Shevchik   goctaprog@gmail.com
+import micropython
+
+from sensor_pack import bus_service
+from sensor_pack.base_sensor import Device, Iterator, check_value
+
+
+class MCP23017(Device, Iterator):
+    """MicroPython class for control 16-Bit I/O Expander with Serial Interface"""
+    def __init__(self, adapter: bus_service.BusAdapter, address: int = 0x27):
+        """eight_bit_mode - если Истина, то два порта (8-бит) ввода/вывода работают отдельно друг от друга.
+        Иначе, два порта (8-бит) ввода/вывода объединяются в один (16 бит) порт ввода/вывода"""
+        s0 = f"Invalid address value: 0x{address:x}!"
+        check_value(address, range(0x20, 0x28), s0)
+        super().__init__(adapter, address, big_byte_order=True)
+        self._bank = True  # то же самое, что и IOCON.BANK. После POR он в нуле! IOCON.BANK = 1
+        #
+        v = self._read_reg(0, 2)    # читаю два байта с последовательных адресов 0 и 1 в адресном пространстве MCP23017
+        if 0xFF == v[0] == v[1]:
+            self._bank = False  # IOCON.BANK = 0
+        self._active_port = 0
+        self._setup()
+
+    # def _read(self, count: int):
+    #    return self.adapter.read(self.address, count)
+
+    @property
+    def active_port(self) -> int:
+        """возвращает текущий порт, над которым производятся операции чтения и записи!"""
+        return self._active_port
+
+    @active_port.setter
+    def active_port(self, value: int):
+        """устанавливает текущий порт, над которым производятся операции чтения и записи!
+        # в режиме 8 бит active_port может принимать значения 0(port a) и 1(port b).
+        # в режиме 16 бит active_port может принимать значение 0."""
+        if self.hex_mode:
+            self._active_port = 0
+            return
+        self._active_port = 1 if value else 0
+
+    @property
+    def hex_mode(self) -> bool:
+        """Возвращает Истина, когда 16-ти битный режим включен. Один порт на 16 бит I/O!
+        Иначе, два порта по 8 бит I/O"""
+        return not self._bank
+
+    @hex_mode.setter
+    def hex_mode(self, value: bool):
+        """Устанавливает режим 16 или 8 бит I/O"""
+        if value:
+            self._bank = False
+            self.active_port = 0
+            # режим один порт(0) 16 bit
+            return
+        self._bank = True
+        # режим два порта(0, 1) по 8 bit
+        self._setup_iocon(bank=self._bank, mirror=False, seqop=False)
+
+    def _read_reg_by_index(self, index: int) -> int:
+        """Чтение регистра по его индексу и текущему активному порту"""
+        addrs = self._get_reg_address(index)    #
+        bytes_count = 2 if self.hex_mode else 1  # кол-во байт
+        res = self._read_reg(addrs[self.active_port], bytes_count)  # bytes
+        fmt = "H" if self.hex_mode else "B"  # формат (H - unsigned short/B - unsigned byte)
+        print(f"DBG addrs: {addrs} bytes_count: {bytes_count} res: {res}")
+        return self.unpack(fmt, res)[0]
+
+    def _write_reg_by_index(self, index: int, value: int):
+        """Запись в регистр по его индексу и текущему активному порту"""
+        addrs = self._get_reg_address(index)  #
+        bytes_count = 2 if self.hex_mode else 1  # кол-во байт
+        print(f"_write_reg: addrs: {addrs}. value: {value:X}")
+        self._write_reg(addrs[self.active_port], value, bytes_count)
+
+    # PULL UP RESISTORS
+    def get_pull_up(self) -> int:
+        """возвращает содержимое регистра GPPU текущего активного порта"""
+        return self._read_reg_by_index(6)   # 6 - GPPU
+
+    def set_pull_up(self, value: int):
+        self._write_reg_by_index(6, value)  # 6 - GPPU
+
+    @property
+    def pull_up(self) -> int:
+        return self.get_pull_up()
+
+    @pull_up.setter
+    def pull_up(self, value):
+        self.set_pull_up(value)
+
+    def get_gpio(self) -> int:
+        return self._read_reg_by_index(9)     # 9 - GPIO
+
+    def set_gpio(self, value: int):
+        self._write_reg_by_index(9, value)  # 9 - GPIO
+
+    @property
+    def gpio(self) -> int:
+        return self.get_gpio()
+
+    @gpio.setter
+    def gpio(self, value):
+        self.set_gpio(value)
+
+    def get_output_latch(self) -> int:
+        """Возвращает значение OLAT.
+        Регистр OLAT обеспечивает доступ к вывода"""
+        return self._read_reg_by_index(0x0A)     # 0x0A - OLAT
+
+    def set_output_latch(self, value: int):
+        """Записывает значение в OLAT"""
+        self._write_reg_by_index(0x0A, value)  # 0x0A - OLAT
+
+    @property
+    def output_latch(self) -> int:
+        return self.get_output_latch()
+
+    @output_latch.setter
+    def output_latch(self, value):
+        self.set_output_latch(value)
+
+    def get_io_dir(self) -> int:
+        """Возвращает значение регистра IODIR.
+        Управляет направлением ввода/вывода данных."""
+        return self._read_reg_by_index(0)     # 0 - IODIR
+
+    def set_io_dir(self, value: int):
+        """Записывает значение в регистр IODIR.
+        Управляет направлением ввода/вывода данных."""
+        self._write_reg_by_index(0, value)  # 0 - IODIR
+
+    @property
+    def io_dir(self) -> int:
+        """Управляет направлением ввода/вывода данных."""
+        return self.get_io_dir()
+
+    @io_dir.setter
+    def io_dir(self, value):
+        self.set_io_dir(value)
+
+    def get_input_pol(self) -> int:
+        """Возвращает значение регистра IPOL.
+        Этот регистр позволяет пользователю настроить полярность соответствующих битов порта GPIO. Если бит установлен,
+        соответствующий бит регистра GPIO будет отражать инвертированное значение на выводе."""
+        return self._read_reg_by_index(1)     # 0 - IPOL
+
+    def set_input_pol(self, value: int):
+        """Записывает значение в регистр IPOL."""
+        self._write_reg_by_index(1, value)  # 0 - IPOL
+
+    @property
+    def input_polarity(self) -> int:
+        """Позволяет пользователю настроить полярность соответствующих битов порта GPIO."""
+        return self.get_input_pol()
+
+    @input_polarity.setter
+    def input_polarity(self, value):
+        self.set_input_pol(value)
+
+    def get_int_en(self) -> int:
+        """Возвращает значение регистра GPINTEN.
+        Регистр GPINTEN управляет функцией прерывания при изменении для каждого вывода."""
+        return self._read_reg_by_index(2)   # GPINTEN: INTERRUPT-ON-CHANGE
+
+    def set_int_en(self, value: int):
+        """Записывает значение в регистр GPINTEN.
+        Регистр GPINTEN управляет функцией прерывания при изменении для каждого вывода."""
+        self._write_reg_by_index(2, value)  # 0 - IPOL
+
+    @property
+    def int_en(self) -> int:
+        """Управляет функцией прерывания при изменении для каждого вывода"""
+        return self.get_int_en()
+
+    @int_en.setter
+    def int_en(self, value: int):
+        self._write_reg_by_index(2, value)  # 0 - IPOL
+
+    def get_int_ctrl(self) -> int:
+        """Возвращаемое значение из регистра INTCON"""
+        return self._read_reg_by_index(4)   # INTCON: INTERRUPT-ON-CHANGE CONTROL REGISTER
+
+    def set_int_ctrl(self, value: int):
+        """Записывает значение в регистр INTCON"""
+        self._write_reg_by_index(4, value)  # 0 - INTCON
+
+    @property
+    def int_ctrl(self) -> int:
+        """Управление прерываниями"""
+        return self.get_int_ctrl()
+
+    @int_ctrl.setter
+    def int_ctrl(self, value: int):
+        self.set_int_ctrl(value)
+
+    def get_def_val(self) -> int:
+        """Возвращает значение из регистра DEFVAL.
+        Значение сравнения по умолчанию настраивается в регистре DEFVAL. Если включено (через GPINTEN и INTCON)
+        сравнение с регистром DEFVAL, противоположное значение на соответствующем выводе вызовет прерывание."""
+        return self._read_reg_by_index(3)
+
+    def set_def_val(self, value: int):
+        """Записывает значение в регистр DEFVAL."""
+        self._write_reg_by_index(3, value)
+
+    @property
+    def def_val(self) -> int:
+        """Значение по умолчанию для сравнения. Формирование прерывания!"""
+        return self.get_def_val()
+
+    @def_val.setter
+    def def_val(self, value: int):
+        self.set_def_val(value)
+
+    def get_if(self) -> int:
+        """Возвращает значение из регистра INTERRUPT FLAG REGISTER"""
+        return self._read_reg_by_index(7)  # INTERRUPT FLAG REGISTER
+
+    def get_int_cap(self) -> int:
+        """Возвращает значение из регистра INTCAP.
+        Регистр INTCAP содержит значение порта GPIO в момент возникновения прерывания."""
+        return self._read_reg_by_index(8)
+
+    @micropython.native
+    def _get_reg_address(self, index: int) -> [tuple, int]:
+        """
+        Возвращает адреса пар(!) регистров по их индексу 0..10 в виде кортежа (адрес регистра А, адрес регистра Б)
+        index,  REG
+        0       IODIR
+        1       IPOL
+        2       GPINTEN
+        3       DEFVAL
+        4       INTCON
+        5       IOCON
+        6       GPPU
+        7       INTF
+        8       INTCAP
+        9       GPIO
+        10      OLAT
+
+        :param index:
+        :return:
+        """
+        s0 = f"Invalid index value: {index}!"
+        check_value(index, range(11), s0)
+        if self._bank:
+            x = index
+            return x, x + 0x10
+        # 16 bit mode
+        x = index << 1
+        return x, x + 1
+
+    def _setup_iocon(self, bank: bool, mirror: bool = False, seqop: bool = False, disslw: bool = False):
+        """Setup IOCON register.
+        Биты  HAEN, ODR, INTPOL обнуляю всегда!"""
+        val = (bank << 7) | (mirror << 6) | (seqop << 5) | (disslw << 4)
+        # self._write_reg(5, value=val)
+        self._write_reg_by_index(5, value=val)
+
+    def _setup_interrupt(self):
+        # запрещаю все, связанное с прерываниями!
+        # Регистр GPINTEN управляет функцией прерывания при изменении для каждого вывода. Если бит установлен,
+        # соответствующий вывод активируется для прерывания по изменению. Регистры DEFVAL и INTCON также должны быть
+        # сконфигурированы, если на каких-либо выводах разрешено прерывание по изменению.
+        pass
+
+    def _read_reg(self, reg_addr: int, bytes_count: int = 1) -> bytes:
+        """Считывает значение из регистра по адресу регистра 0..0x10. Смотри _get_reg_address"""
+        return self.adapter.read_register(self.address, reg_addr, bytes_count)
+
+    def _write_reg(self, reg_addr: int, value: int, bytes_count: int = 1):
+        """Записывает в регистр с адресом reg_addr значение value по шине."""
+        bo = self._get_byteorder_as_str()[0]
+        self.adapter.write_register(self.address, reg_addr, value, bytes_count, bo)   # !!!
+
+    def _setup(self):
+        """Настройка портов на ввод/вывод. По умолчанию выводы P0..P7 настраиваются как ВХОДЫ!
+        Если бит установлен в ноль, то происходит "подтяжка" вывода порта P0..P7 к земле через внутренний транзистор!
+        Если бит установлен в единицу, вывод порта P0..P7 будет подтянут к питанию через источник тока в 100 uA!
+
+        Поэтому:
+            * если вы подключаете к выводу порта P0..P7 кнопку, то записывайте в соотв. бит ЕДИНИЦУ и потом ЧИТАЙТЕ
+              состояние, а кнопку подключайте между выводом порта и ЗЕМЛЕЙ(VSS)!!!
+            * если вы подключаете к выводу порта P0..P7 нагрузку, то подавать напряжение на нее нужно ЗАПИСЬЮ в
+              соответствующий бит НУЛЯ, а нагрузку подключайте между +Питание(VDD) и выводом порта P0..P7!!!"""
+        self.hex_mode = False
+
+    def __call__(self):
+        """Чтение состояния линий портов Pх0..Pх7"""
+        return self.gpio
+
+    def __iter__(self):
+        return self
+
+    def __next__(self) -> int:
+        """Можно использовать как итератор (чтение в цикле for)"""
+        return self.gpio
